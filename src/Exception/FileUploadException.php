88a6ff9c3e61161cc751bfc2991a62bae31abfc1
jmsche <contact@jmsche.fr>
2021-06-02 16:50:10 +0200

    Add php-cs-fixer & apply suggested code style

--- src/Exception/FileUploadException.php
+++ src/Exception/FileUploadException.php
@@ -1,9 +1,9 @@
 <?php
+
 namespace Proglab\SftpClientBundle\Exception;
 
 use Psr\Container\ContainerExceptionInterface;
 
 class FileUploadException extends \InvalidArgumentException implements ContainerExceptionInterface
 {
-
 }
