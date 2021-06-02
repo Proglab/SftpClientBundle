e4e61b7704296af4c6d039e24c8cb94db6b75f6a
Fabrice <info@proglab.com>
2021-06-02 14:05:41 +0200

    :recycle: little refacto

--- src/Service/SftpClient.php
+++ src/Service/SftpClient.php
@@ -9,87 +9,53 @@ use Proglab\SftpClientBundle\Exception\InvalidArgumentException;
 use Psr\Log\LoggerInterface;
 
 /**
- * Sftp Client
- * @package Proglab\SftpClientBundle\Service
+ * Sftp Client.
  */
-class SftpClient {
-    /** @var null|resource */
+class SftpClient
+{
+    /** @var resource|null */
     private $sftp;
     private LoggerInterface $logger;
     /** @var false|resource */
     private $connection;
 
-    public function __construct( LoggerInterface $logger) {
+    public function __construct(LoggerInterface $logger)
+    {
         $this->logger = $logger;
     }
 
     /**
-     * Connect to an SFTP
+     * Connect to an SFTP.
      *
-     * @param string $serverUsername
-     * @param string $serverPassword
-     * @param string $host
-     * @param int $serverPort
-     * @return $this
+     * @param string $serverUsername server username
+     * @param string $serverPassword server password
+     * @param string $host server host
+     * @param int $serverPort server port (22 by default)
+     * @return self
      */
     public function connect(string $serverUsername, string $serverPassword, string $host, int $serverPort = 22): self
     {
         $this->connection = ssh2_connect($host, $serverPort);
-        if (!ssh2_auth_password($this->connection, $serverUsername, $serverPassword))
-        {
+        if (!ssh2_auth_password($this->connection, $serverUsername, $serverPassword)) {
             $this->logger->emergency('Cannot connect to server '.$host.':'.$serverPort);
             throw new InvalidArgumentException('Cannot connect to server '.$host.':'.$serverPort);
         }
         $this->sftp = ssh2_sftp($this->connection);
+
         return $this;
     }
 
     /**
-     * Download a file
-     *
-     * @param string $fileRemotePath
-     * @param string $fileLocalPath
-     * @param bool $delete
-     * @return bool
-     */
-    public function download(string $fileRemotePath, string $fileLocalPath, bool $delete = true): bool
-    {
-        $this->logger->debug('download '.$fileRemotePath.' to '.$fileLocalPath);
-        $stream = fopen("ssh2.sftp://" . (int) $this->sftp . $fileRemotePath, 'rb');
-        if (! $stream) {
-            $this->logger->emergency("Could not open file: ".$fileRemotePath);
-            throw new FileException("Could not open file: ");
-        }
-        $contents = stream_get_contents($stream);
-        @fclose($stream);
-        file_put_contents($fileLocalPath, $contents);
-        if (filesize ("ssh2.sftp://" . (int) $this->sftp . $fileRemotePath) === filesize ($fileLocalPath))
-        {
-            if ($delete === 1)
-            {
-                unlink("ssh2.sftp://" . (int) $this->sftp . $fileRemotePath);
-            }
-
-            return true;
-        }
-
-        throw new FileDownloadException($fileLocalPath.' and '.$fileRemotePath.' d\'ont have the same size');
-    }
-
-    /**
-     * list remote directory
+     * list remote directory.
      *
      * @param string $dir The dir (begin by /)
-     * @return array
      */
     public function getRemoteListFiles(string $dir): array
     {
         $return = [];
-        $dirs = scandir("ssh2.sftp://" . (int) $this->sftp . $dir);
-        foreach($dirs as $d)
-        {
-            if ($d !== '.' && $d !== '..')
-            {
+        $dirs = scandir('ssh2.sftp://'.(int) $this->sftp.$dir);
+        foreach ($dirs as $d) {
+            if ('.' !== $d && '..' !== $d) {
                 $return[] = $d;
             }
         }
@@ -98,41 +64,57 @@ class SftpClient {
     }
 
     /**
-     * list local directory
-     *
-     * @param string $localDir
-     * @return array
+     * list local directory.
      */
     public function getLocalListFiles(string $localDir): array
     {
         $localFiles = glob($localDir.'*.*');
-        foreach($localFiles as $key => $file)
-        {
+        foreach ($localFiles as $key => $file) {
             $localFiles[$key] = basename($file);
         }
 
         return $localFiles;
     }
 
+
     /**
-     * upload a file
-     *
-     * @param string $fileLocalPath
-     * @param string $fileRemotePath
-     * @param bool $delete
-     * @return bool
+     * Download a file.
      */
-    public function upload(string $fileLocalPath, string $fileRemotePath, bool $delete = true): bool
+    public function download(string $fileRemotePath, string $fileLocalPath, bool $delete = false): bool
+    {
+        $this->logger->debug('download '.$fileRemotePath.' to '.$fileLocalPath);
+        $stream = fopen('ssh2.sftp://'.(int) $this->sftp.$fileRemotePath, 'rb');
+        if (!$stream) {
+            $this->logger->emergency('Could not open file: '.$fileRemotePath);
+            throw new FileException('Could not open file: ');
+        }
+        $contents = stream_get_contents($stream);
+        @fclose($stream);
+        file_put_contents($fileLocalPath, $contents);
+        if (filesize('ssh2.sftp://'.(int) $this->sftp.$fileRemotePath) === filesize($fileLocalPath)) {
+            if (true === $delete) {
+                unlink('ssh2.sftp://'.(int) $this->sftp.$fileRemotePath);
+            }
+
+            return true;
+        }
+
+        throw new FileDownloadException($fileLocalPath.' and '.$fileRemotePath.' d\'ont have the same size');
+    }
+
+
+    /**
+     * upload a file.
+     */
+    public function upload(string $fileLocalPath, string $fileRemotePath, bool $delete = false): bool
     {
         $this->logger->debug('upload '.$fileLocalPath.' to '.$fileRemotePath);
-        $stream = fopen("ssh2.sftp://" . (int)$this->sftp . $fileRemotePath, 'wb+');
+        $stream = fopen('ssh2.sftp://'.(int) $this->sftp.$fileRemotePath, 'wb+');
         $dataToSend = file_get_contents($fileLocalPath);
         fwrite($stream, $dataToSend);
         fclose($stream);
-        if (filesize ("ssh2.sftp://" . (int)$this->sftp . $fileRemotePath) === filesize ($fileLocalPath))
-        {
-            if ($delete === 1)
-            {
+        if (filesize('ssh2.sftp://'.(int) $this->sftp.$fileRemotePath) === filesize($fileLocalPath)) {
+            if (true === $delete) {
                 unlink($fileLocalPath);
             }
 
@@ -142,20 +124,14 @@ class SftpClient {
     }
 
     /**
-     * Synchronise Local files to remote directory
-     *
-     * @param string $localDir
-     * @param string $remoteDir
-     * @param bool $delete
-     * @return array
+     * Synchronise Local files to remote directory.
      */
     public function syncLocalDirToRemote(string $localDir, string $remoteDir, bool $delete = true): array
     {
         $localFiles = $this->getLocalListFiles($localDir);
         $remoteFiles = $this->getRemoteListFiles($remoteDir);
         $file_to_upload = array_diff($localFiles, $remoteFiles);
-        foreach($file_to_upload as $file)
-        {
+        foreach ($file_to_upload as $file) {
             $this->upload($localDir.$file, $remoteDir.$file, $delete);
         }
 
@@ -163,20 +139,14 @@ class SftpClient {
     }
 
     /**
-     * Synchronise remote directory to local files
-     *
-     * @param string $remoteDir
-     * @param string $localDir
-     * @param bool $delete
-     * @return array
+     * Synchronise remote directory to local files.
      */
     public function syncRemoteDirToLocal(string $remoteDir, string $localDir, bool $delete = true): array
     {
         $localFiles = $this->getLocalListFiles($localDir);
         $remoteFiles = $this->getRemoteListFiles($remoteDir);
         $file_to_download = array_diff($remoteFiles, $localFiles);
-        foreach($file_to_download as $file)
-        {
+        foreach ($file_to_download as $file) {
             $this->download($remoteDir.$file, $localDir.$file, $delete);
         }
 
@@ -184,15 +154,15 @@ class SftpClient {
     }
 
     /**
-     * stop connection to the SFTP
+     * stop connection to the SFTP.
      */
     public function deco(): void
     {
-        $this->connection = null;
+        $this->connection = false;
     }
 
-    public function __destruct() {
+    public function __destruct()
+    {
         $this->deco();
     }
-
 }
